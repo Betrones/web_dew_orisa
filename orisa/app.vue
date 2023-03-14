79ef7dff6a4bb858b2ac847d8423486621fa9b06
Betrones <betronesdorg@gmail.com>
2023-03-14 22:06:05 +0400

    init

---
+++ orisa/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <section class="sights">
+    <div class="sight_list">123321</div>
+    <div class="sight_chosen">123321</div>
+  </section>
+</template>
---
+++ orisa/nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default {
+    build: {
+      postcss: {
+        postcssOptions: {
+          plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+          },
+        },
+      },
+    },
+
+    css: [
+        '@/assets/css/global.scss',
+      ],
+  }
